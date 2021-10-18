f2bb5a3d71ed739884bee8fb75bbd093c5163ac9
Basil Crow <me@basilcrow.com>
2021-10-18 11:11:11 -0700

    Use try-with-resources where possible

--- src/test/java/org/jenkinsci/plugins/database/sqlite/PerItemSQLiteDatabaseTest.java
+++ src/test/java/org/jenkinsci/plugins/database/sqlite/PerItemSQLiteDatabaseTest.java
@@ -5,7 +5,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Arrays;
-import javax.sql.DataSource;
 import org.apache.commons.dbcp2.PoolingDataSource;
 import static org.hamcrest.CoreMatchers.is;
 import org.jenkinsci.plugins.database.PerItemDatabaseConfiguration;
@@ -20,36 +19,21 @@ public class PerItemSQLiteDatabaseTest {
 
     @Test public void basics() throws Exception {
         FreeStyleProject p = j.jenkins.createProject(FreeStyleProject.class, "somejob");
-        DataSource ds = PerItemDatabaseConfiguration.find().getDataSource(p);
-        Connection con = ds.getConnection();
-        try {
-            Statement st = con.createStatement();
-            try {
-                st.execute("create table FOO (a int, b int )");
-            } finally {
-                st.close();
-            }
-            st = con.createStatement();
-            try {
-                st.execute("insert into FOO values (1,2)");
-            } finally {
-                st.close();
-            }
-            st = con.createStatement();
-            try {
-                ResultSet r = st.executeQuery("select b from FOO where a=1");
-                try {
-                    r.next();
-                    assertThat(r.getInt(1), is(2));
-                } finally {
-                    r.close();
+        try (PoolingDataSource<?> ds = (PoolingDataSource<?>) PerItemDatabaseConfiguration.find().getDataSource(p)) {
+            try (Connection con = ds.getConnection()) {
+                try (Statement st = con.createStatement()) {
+                    st.execute("create table FOO (a int, b int )");
                 }
-            } finally {
-                st.close();
+                try (Statement st = con.createStatement()) {
+                    st.execute("insert into FOO values (1,2)");
+                }
+                try (Statement st = con.createStatement()) {
+                    try (ResultSet r = st.executeQuery("select b from FOO where a=1")) {
+                        r.next();
+                        assertThat(r.getInt(1), is(2));
+                    }
+                }
             }
-        } finally {
-            con.close();
-            ((PoolingDataSource<?>) ds).close();
         }
         System.err.println("XXX " + Arrays.asList(p.getRootDir().list()));
     }
