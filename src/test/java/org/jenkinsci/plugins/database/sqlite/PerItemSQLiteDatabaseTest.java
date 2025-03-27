9f2ac28db1e370b7af707e23a0422a64795ee01e
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-03-27 12:58:43 +0100

    Migrate tests to JUnit5
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods
    * Minor code cleanup

--- src/test/java/org/jenkinsci/plugins/database/sqlite/PerItemSQLiteDatabaseTest.java
+++ src/test/java/org/jenkinsci/plugins/database/sqlite/PerItemSQLiteDatabaseTest.java
@@ -7,38 +7,30 @@ import hudson.model.FreeStyleProject;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.Arrays;
 import org.apache.commons.dbcp2.PoolingDataSource;
 import org.jenkinsci.plugins.database.PerItemDatabaseConfiguration;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 
-public class PerItemSQLiteDatabaseTest {
-
-    @Rule
-    public JenkinsRule j = new JenkinsRule();
+@WithJenkins
+class PerItemSQLiteDatabaseTest {
 
     @Test
-    public void basics() throws Exception {
+    void basics(JenkinsRule j) throws Exception {
         FreeStyleProject p = j.jenkins.createProject(FreeStyleProject.class, "somejob");
-        try (PoolingDataSource<?> ds =
-                (PoolingDataSource<?>) PerItemDatabaseConfiguration.find().getDataSource(p)) {
-            try (Connection con = ds.getConnection()) {
-                try (Statement st = con.createStatement()) {
-                    st.execute("create table FOO (a int, b int )");
-                }
-                try (Statement st = con.createStatement()) {
-                    st.execute("insert into FOO values (1,2)");
-                }
-                try (Statement st = con.createStatement()) {
-                    try (ResultSet r = st.executeQuery("select b from FOO where a=1")) {
-                        r.next();
-                        assertThat(r.getInt(1), is(2));
-                    }
-                }
+        try (PoolingDataSource<?> ds = (PoolingDataSource<?>)
+                        PerItemDatabaseConfiguration.find().getDataSource(p);
+                Connection con = ds.getConnection();
+                Statement st = con.createStatement()) {
+
+            st.execute("create table FOO (a int, b int )");
+            st.execute("insert into FOO values (1,2)");
+
+            try (ResultSet r = st.executeQuery("select b from FOO where a=1")) {
+                r.next();
+                assertThat(r.getInt(1), is(2));
             }
         }
-        System.err.println("XXX " + Arrays.asList(p.getRootDir().list()));
     }
 }
